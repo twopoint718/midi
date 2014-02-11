58fda0c4063c04864f797fc08a2edf53efb6d201
Jon Childress <jon@childr.es>
2014-02-11 08:56:55 -0600

    final reorg and beginning test suite

--- src/midi/deltaTime.hs
+++ src/midi/deltaTime.hs
@@ -1,4 +1,6 @@
-module Bit.Variable where
+module Midi.DeltaTime
+( toMultiByte
+) where
 
 import Data.Word (Word8)
 
@@ -16,11 +18,12 @@ import Data.Word (Word8)
 -}
 
 -- TODO: Use a ShowS pattern instead of ++
-multibyte :: Int -> [Word8]
-multibyte n = bytes [] n 0
+toMultiByte :: Int -> [Word8]
+toMultiByte n = bytes [] n 0
   where
     bytes words remain pow
       | remain >= 128^(pow+1) = bytes words remain (pow+1)
-      | pow > 0 = bytes (words ++ [fromIntegral $ 128+times]) rem (pow-1)
+      | pow > 0 = bytes (words ++ [fromIntegral $ 128+t]) r (pow-1)
       | otherwise = words ++ [fromIntegral remain]
-      where (times,rem) = divMod remain $ 128^pow
+      where (t,r) = divMod remain $ 128^pow
+
